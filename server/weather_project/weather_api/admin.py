cf87f255126eef6ec004d6fbff673955179ff167
Lilian Luca <lilianluca2118@gmail.com>
2024-05-11 00:14:15 +0200

    Setup docker

--- server/weather_project/weather_api/admin.py
+++ server/weather_project/weather_api/admin.py
@@ -5,3 +5,4 @@ Django Admin Configuration File.
 # from django.contrib import admin
 
 # Register your models here.
+
--- server/weather_project/weather_project/settings.py
+++ server/weather_project/weather_project/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +29,7 @@ SECRET_KEY = "django-insecure-m*4iyim)!ouz6rrp)_yvgi5@&zs%$p&rz@r5+o)ox5a6*l1mi#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -75,8 +79,12 @@ WSGI_APPLICATION = "weather_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
     }
 }
 
