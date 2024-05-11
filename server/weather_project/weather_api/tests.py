3105d84d706198a37170a33911a075690ee41c31
Lilian Luca <lilianluca2118@gmail.com>
2024-05-11 13:28:15 +0200

    Added tests for weather_api app

--- server/weather_project/weather_api/tests.py
+++ server/weather_project/weather_api/tests.py
@@ -2,6 +2,17 @@
 Contains unit tests for the Weather API App.
 """
 
+import unittest
+
 # from django.test import TestCase
 
+
 # Create your tests here.
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+
+    def test_isupper(self):
+        self.assertTrue("FOO".isupper())
+        self.assertFalse("Foo".isupper())
