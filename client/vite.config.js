4ff18a348915fa26f2ab9f3aef0324e73324a74a
Lilian Luca <lilianluca2118@gmail.com>
2024-05-18 13:08:44 +0200

    Deployment configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,14 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        // target: 'http://127.0.0.1:8000/',
-        target: '/choreo-apis/djangorestapi/serverweatherproject/rest-api-be2/v1',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://127.0.0.1:8000/',
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //   },
+  // },
 });
