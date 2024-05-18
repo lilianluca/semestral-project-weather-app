93f32d0f88dc5dd181d89fbddeeeccff0e401c52
Lilian Luca <lilianluca2118@gmail.com>
2024-05-18 14:13:50 +0200

    Deployment configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://127.0.0.1:8000/',
-  //       changeOrigin: true,
-  //       secure: false,
-  //     },
-  //   },
-  // },
+  server: {
+    proxy: {
+      '/api': {
+        // target: 'http://127.0.0.1:8000/',
+        target: 'https://weather-rest-api-878f.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
